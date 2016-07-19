24e7173d7a6938a267f74b5f82858f1e8d513265
Ka_Lenovo <heykamok@gmail.com>
2016-07-18 23:44:26 -0400

    Add upvotes desc

--- public/assets/javascripts/controller.js
+++ public/assets/javascripts/controller.js
@@ -1,3 +1,10 @@
 app.controller('mainCtrl', function($scope) {
 	$scope.test = "Hello world!";
+	$scope.posts = [
+	{title: 'post 1', upvotes: 5},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 15},
+  {title: 'post 4', upvotes: 9},
+  {title: 'post 5', upvotes: 4}
+	];
 });
